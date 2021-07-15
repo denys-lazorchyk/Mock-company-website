a31729aab7cfce71239820733d7f310cd1ff01e4
denys-lazorchyk <denyslazorchyk@gmail.com>
2021-07-16 01:41:39 +0200

    Initial commit

---
+++ code.js
@@ -0,0 +1,11 @@
+let intervalID;
+intervalID = setInterval(() => {
+  let image = document.querySelector(".lorem-me").clientHeight;
+  document.querySelector(".image").style.height = `${image}px`;
+}, 100);
+
+window.addEventListener("beforeunload", function (e) {
+  e.preventDefault();
+  e.returnValue = "";
+  clearInterval(intervalID);
+});
