9b68a67e8383aecc4f5384db2d199540147284dc
denys-lazorchyk <denyslazorchyk@gmail.com>
2021-08-15 22:49:37 +0300

    commit

--- code.js
+++ code.js
@@ -1,11 +1,17 @@
+const submit = document.querySelector("#submit");
+
 let intervalID;
 intervalID = setInterval(() => {
-  let image = document.querySelector(".lorem-me").clientHeight;
-  document.querySelector(".image").style.height = `${image}px`;
+	let image = document.querySelector(".lorem-me").clientHeight;
+	document.querySelector(".image").style.height = `${image}px`;
 }, 100);
 
 window.addEventListener("beforeunload", function (e) {
-  e.preventDefault();
-  e.returnValue = "";
-  clearInterval(intervalID);
+	e.preventDefault();
+	e.returnValue = "";
+	clearInterval(intervalID);
+});
+
+submit.addEventListener("click", (e) => {
+	e.preventDefault();
 });
