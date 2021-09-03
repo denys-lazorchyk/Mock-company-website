4c7fc00f118b41c692031bfda9af109b9910ba24
denys-lazorchyk <denyslazorchyk@gmail.com>
2021-09-03 18:44:24 +0300

    commit

--- code.js
+++ code.js
@@ -5,7 +5,9 @@ const bookMeet = document.querySelector(".btn");
 let intervalID;
 intervalID = setInterval(() => {
 	let image = document.querySelector(".lorem-me").clientHeight;
-	document.querySelector(".image").style.height = `${image}px`;
+	if (window.screen.width > 1050) {
+		document.querySelector(".image").style.height = `${image}px`;
+	}
 }, 100);
 
 window.addEventListener("beforeunload", function (e) {
